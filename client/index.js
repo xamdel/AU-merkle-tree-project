a742609361afb35df750cc11e9b90ec683ec2aa1
xamdel <xamdel@protonmail.com>
2023-03-26 14:42:30 -0700

    First commit

--- client/index.js
+++ client/index.js
@@ -1,17 +1,51 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const readline = require("readline");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+// Set up node.js builtin readline for getting user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Function for getting user input
+function getInput() {
+  return new Promise((resolve) => {
+    rl.question(
+      'Enter a name: ',
+      (name) => {
+        resolve(name);
+      }
+    );
+  });
+}
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
 
+  // Create a merkle tree for the nicelist
+  const niceListMerkleTree = new MerkleTree(niceList);
+
+  // console.log(`Merkle Root: ${niceListMerkleTree.getRoot()}`);
+
+  // Get name from user
+  let name = await getInput();
+
+  // Get index of entered name
+  const nameIndex = niceList.indexOf(name);
+
+  // Generate proof and send to server
+  const proof = niceListMerkleTree.getProof(nameIndex);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof,
   });
 
-  console.log({ gift });
+  console.log(gift);
+  process.exit(0);
 }
 
 main();
--- server/index.js
+++ server/index.js
@@ -8,19 +8,26 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = 'eae5c5fb1e590005bc3115640f6d93ef14a5c3df6a42e51d6bc3425f9a3d91d9';
+
+const futuramaRobotSantaQuotes = ["You've been very naughty. Very naughty indeed!", "Ho, ho, ho. It's time to get jolly on your naughty asses!", "I'm going to shove coal so far up your stocking you'll be coughing up diamonds!"]
+
+function getRandomQuote() {
+  const index = Math.floor(Math.random() * futuramaRobotSantaQuotes.length);
+  return futuramaRobotSantaQuotes[index];
+}
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
   if(isInTheList) {
-    res.send("You got a toy robot!");
+    res.send("You get a pogo stick!");
   }
   else {
-    res.send("You are not on the list :(");
+    res.send(getRandomQuote());
   }
 });
 
